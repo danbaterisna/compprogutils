e5c358695e2e521ee4e9545b43ea2451e92b8053
Dan Baterisna <dan.alden.baterisna@gmail.com>
2020-05-19 22:41:21 +0800

    Added parsing capabilities

---
+++ parsers.py
@@ -0,0 +1,32 @@
+
+import os
+
+import cpu_errors
+
+WHITESPACE = "\n\r\t "
+
+class StringParser:
+    """Tool for extracting data from a string."""
+    def __init__(self, strng):
+        self.inString = strng
+        self.curID = 0
+    def readUntil(self, charSet = WHITESPACE, skipOver = True):
+        """ Read until a character in charSet is found. By default, this is whitespace.
+        Skip over this character, unless skipOver is false.
+        Return the read string. """
+        charsRead = []
+        while self.curID < len(self.inString):
+            cChar = self.inString[self.curID]
+            if cChar in charSet:
+                if skipOver:
+                    self.curID += 1
+                    break
+            charsRead.append(cChar)
+            self.curID += 1
+        return ''.join(charsRead)
+    def readDataFromString(self, dataType, delineator = WHITESPACE, skipOver = True):
+        """ Read a single member of dataType from the string. dataType should have
+        a constructor that accepts a string. Raises ValueError if it is not found. """
+        possibleInt = self.readUntil(delineator, skipOver)
+        return dataType(possibleInt)
+
