a2914a8041fe7c6eba8555ae01ed689a5ba39f96
Dan Baterisna <dan.alden.baterisna@gmail.com>
2020-05-23 19:13:05 +0800

    Fixed reading of checker output

--- checkers.py
+++ checkers.py
@@ -12,7 +12,7 @@ class Checker(executables.Executable):
     """ A checker is an executable that validates test output. """
     def checkOutputFile(self, test, outputFile):
         """ Check the outputFile file against the given test. Return a (score, message) tuple. """
-        checkerOutput = self.run(cmdArgs = [test.getFilename(tests.TestFile.INPUT), test.getFilename(tests.TestFile.OUTPUT), \
+        checkerOutput, checkerTime = self.run(cmdArgs = [test.getFilename(tests.TestFile.INPUT), test.getFilename(tests.TestFile.OUTPUT), \
                         test.getFilename(tests.TestFile.DATA), outputFile])
         outputToParse = parsers.StringParser(checkerOutput)
         score = outputToParse.readDataFromString(float)
