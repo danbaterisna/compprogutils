dbd6660fa1c33567dc303c6eee5c5014ce681743
Dan Baterisna <dan.alden.baterisna@gmail.com>
2020-05-25 18:33:16 +0800

    Rewrote StringParser interface

--- parsers.py
+++ parsers.py
@@ -1,7 +1,6 @@
-
 import os
 
-from . import cpu_errors
+import compprogutils.cpu_errors as cpu_errors
 
 WHITESPACE = "\n\r\t "
 
@@ -10,19 +9,26 @@ class StringParser:
     def __init__(self, strng):
         self.inString = strng
         self.curID = 0
+    def curChar(self, moveToNext = False):
+        if self.curID < len(self.inString):
+            retVal = self.inString[self.curID]
+            if moveToNext:
+                self.curID += 1
+            return retVal
+        else:
+            raise cpu_errors.UnexpectedEOF(f"""EOF on attempted char find""")
     def readUntil(self, charSet = WHITESPACE, skipOver = True):
         """ Read until a character in charSet is found. By default, this is whitespace.
         Skip over this character, unless skipOver is false.
         Return the read string. """
         charsRead = []
-        while self.curID < len(self.inString):
-            cChar = self.inString[self.curID]
+        while self.curChar() is not None:
+            cChar = self.curChar()
             if cChar in charSet:
                 if skipOver:
-                    self.curID += 1
-                    break
-            charsRead.append(cChar)
-            self.curID += 1
+                    self.curChar(moveToNext = True)
+                break
+            charsRead.append(self.curChar(moveToNext = True))
         return ''.join(charsRead)
     def readDataFromString(self, dataType, charSet = WHITESPACE, skipOver = True):
         """ Read a single member of dataType from the string. dataType should have
