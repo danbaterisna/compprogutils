6a27a83ca3a1b7184e88bb22c6ca34c32692beaa
Dan Baterisna <dan.alden.baterisna@gmail.com>
2020-05-25 19:02:18 +0800

    Add file parsing and tokenizing

--- parsers.py
+++ parsers.py
@@ -30,9 +30,38 @@ class StringParser:
                 break
             charsRead.append(self.curChar(moveToNext = True))
         return ''.join(charsRead)
-    def readDataFromString(self, dataType, charSet = WHITESPACE, skipOver = True):
+    def readData(self, dataType, charSet = WHITESPACE, skipOver = True):
         """ Read a single member of dataType from the string. dataType should have
         a constructor that accepts a string. Raises ValueError if it is not found. """
         possibleInt = self.readUntil(charSet, skipOver)
         return dataType(possibleInt)
+
+class FileParser(StringParser):
+    """ Tool for extracting data from a file. """
+    def __init__(self, byteStream):
+        self.byteStream = byteStream
+        self.streamHead = None
+    def curChar(self, moveToNext = False):
+        if self.streamHead is None:
+            self.streamHead = self.byteStream.read(1)
+        retVal = self.streamHead
+        if retVal == "":
+            raise cpu_errors.UnexpectedEOF(f"""EOF on attempted char find""")
+        if moveToNext:
+            self.streamHead = self.byteStream.read(1)
+        return retVal
+
+def tokenize(fileStream, removeEmptyTokens = True):
+    """ Return all the whitespace-separated tokens of the file in fileStream. If removeEmptyTokens
+    is true, all empty tokens are deleted from the final list."""
+    fp = FileParser(fileStream)
+    tokensRead = []
+    while True:
+        try:
+            tokensRead.append(fp.readData(str))
+        except cpu_errors.UnexpectedEOF as e:
+            break
+    if removeEmptyTokens:
+        tokensRead = [token for token in tokensRead if token != ""]
+    return tokensRead
 
