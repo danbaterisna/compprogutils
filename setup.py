19148456e9eae6f0149a5340433efbce023f6d29
Dan Baterisna <dan.alden.baterisna@gmail.com>
2020-05-27 23:05:00 +0800

    Added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="compprogutils",
+    version=0.1,
+    author="Dan Baterisna",
+    author_email="dan.alden.baterisna@gmail.com",
+    description="Utilities for streamlining common operations in programming contests."
+)
